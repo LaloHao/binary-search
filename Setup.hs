4dfe1390ab1b9a12a09fbd14df020eac20faca24
Eduardo V <lalohao@gmail.com>
2020-12-07 14:25:59 -0700

    FIX: Building for >=GHC8.8.2

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,11 @@
+{-# LANGUAGE CPP #-}
 import Distribution.Simple
+#ifdef MIN_VERSION_cabal
+#if MIN_VERSION_cabal(1,3,10)
 main = defaultMainWithHooks defaultUserHooks
+#else
+main = defaultMain
+#endif
+#else
+main = defaultMain
+#endif
